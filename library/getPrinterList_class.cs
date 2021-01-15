3c0e7252e1aa6ccb851247fc1034386f1a47594b
Jave Lupango <58350420+PanggaJave@users.noreply.github.com>
2021-01-15 09:56:35 +0800

    get list of printer name in your PC using C#
    get list of printer name in your PC using C#

---
+++ library/getPrinterList_class.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Printing;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp1.library
+{
+    class getPrinterList_class
+    {
+        /// <summary>
+        /// get printer List only Printer Name
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable getprinterlist()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("PrinterName");
+            foreach (String printer in PrinterSettings.InstalledPrinters)
+            {
+                dt.Rows.Add(printer.ToString());
+                // comboBox2.Items.Add(printer.ToString());
+            }
+            return dt; //JsonConvert.SerializeObject(dt);
+        }
+
+        /// <summary>
+        /// Get printer List all
+        /// </summary>
+        /// <returns></returns>
+        public static string getprinterlistV2()
+        {            
+            return JsonConvert.SerializeObject(PrinterSettings.InstalledPrinters);
+        }
+    }
+}
